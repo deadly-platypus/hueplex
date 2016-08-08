ad50b763124e1216b54fc185d545b38e99a6abfc
Derrick McKee <derrick.mckee@gmail.com>
2016-08-07 20:57:20 -0400

    initial commit and gitignore additions

---
+++ src/hueplex/ColorSample.java
@@ -0,0 +1,24 @@
+package hueplex;
+
+import java.awt.Color;
+
+public class ColorSample {
+  protected Color rgb;
+  protected Integer brightness;
+
+  public Color getRgb() {
+    return rgb;
+  }
+
+  public void setRgb(Color rgb) {
+    this.rgb = rgb;
+  }
+
+  public Integer getBrightness() {
+    return brightness;
+  }
+
+  public void setBrightness(Integer brightness) {
+    this.brightness = brightness;
+  }
+}
---
+++ src/hueplex/interfaces/LightStateProvider.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package hueplex.interfaces;
+
+import com.philips.lighting.model.PHLightState;
+
+/**
+ * @author Derrick
+ * 
+ *         Any media that can change a light must implement these methods. A LightStateProvider is
+ *         intended to be some kind of media that can be displayed on the TV. This could be music,
+ *         movies, TV shows, or whatever.
+ */
+public interface LightStateProvider {
+  /**
+   * @return the latest real-time computed {@link PHLightState}
+   */
+  PHLightState getCurrentLightState();
+
+  /**
+   * Change the current {@link PHLightState}
+   */
+  void updateLightState();
+}
